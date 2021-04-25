b7e3247f2974e953869b0d50abcb9d5bf4fbe163
Lenny Lopes <lennymlopes@gmail.com>
2021-04-25 21:13:45 +0200

    replace else with guard case

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,9 @@ window.addEventListener('scroll', () => {
   let sections = document.querySelectorAll('section')
   let position = window.innerHeight
   for(section of sections) {
-    
+    section.classList.remove('active')
     if(section.getBoundingClientRect().bottom / 0.9 < position) {
       section.classList.add('active')
-    } else {
-      section.classList.remove('active')
-    }
+    } 
   }
 })
