49528fb45d9415d76c04fd271ced15b39b4caee9
Lenny Lopes <lennymlopes@gmail.com>
2021-05-02 14:26:36 +0200

    remove unneeded js

--- static/js/main.js
+++ static/js/main.js
@@ -2,18 +2,11 @@ window.addEventListener('scroll', () => {
   let sections = document.querySelectorAll('.fade')
   let position = window.innerHeight
   for(section of sections) {
-    let graphs = section.querySelectorAll('.bar')
     if(section.getBoundingClientRect().bottom / 0.9 < position) {
       section.classList.add('active')
-      for(graph of graphs) {
-        graph.classList.add('active')
-      }
     } 
     else if (section.getBoundingClientRect().top / 0.5 > position) {
       section.classList.remove('active')
-      for(graph of graphs) {
-        graph.classList.remove('active')
-      }
     }
   }
 })
