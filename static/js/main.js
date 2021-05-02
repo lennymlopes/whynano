554b6e56fc77cce2f7257604c0c85a1d7bd628ff
Lenny Lopes <lennymlopes@gmail.com>
2021-05-02 13:22:10 +0200

    simplify graph animation

--- static/js/main.js
+++ static/js/main.js
@@ -2,27 +2,19 @@ window.addEventListener('scroll', () => {
   let sections = document.querySelectorAll('.fade')
   let position = window.innerHeight
   for(section of sections) {
-    
+    let graphs = section.querySelectorAll('.bar')
     if(section.getBoundingClientRect().top / 0.6 < position) {
       section.classList.add('active')
+      for(graph of graphs) {
+        graph.classList.add('active')
+      }
     } 
     else if (section.getBoundingClientRect().top / 0.7 > position) {
       section.classList.remove('active')
+      for(graph of graphs) {
+        graph.classList.remove('active')
+      }
     }
   }
 })
 
-window.addEventListener('scroll', () => {
-  let graphs = document.querySelectorAll('.bar')
-  let position = window.innerHeight
-  for(graph of graphs) {
-    
-    if(graph.getBoundingClientRect().bottom / 0.9 < position) {
-      graph.classList.add('active')
-      // graph.innerHeight = "50rem"
-    } 
-    else if (graph.getBoundingClientRect().top / 0.7 > position) {
-      graph.classList.remove('active')
-    }
-  }
-})
