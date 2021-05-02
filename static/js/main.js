e53c7c038ef33745669be6858d1d5711ba108ed7
Lenny Lopes <lennymlopes@gmail.com>
2021-05-02 13:44:31 +0200

    fade experiments

--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,13 @@ window.addEventListener('scroll', () => {
   let position = window.innerHeight
   for(section of sections) {
     let graphs = section.querySelectorAll('.bar')
-    if(section.getBoundingClientRect().top / 0.6 < position) {
+    if(section.getBoundingClientRect().bottom / 0.9 < position) {
       section.classList.add('active')
       for(graph of graphs) {
         graph.classList.add('active')
       }
     } 
-    else if (section.getBoundingClientRect().top / 0.7 > position) {
+    else if (section.getBoundingClientRect().top / 0.5 > position) {
       section.classList.remove('active')
       for(graph of graphs) {
         graph.classList.remove('active')
