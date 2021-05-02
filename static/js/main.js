ba8af6ff27991d88a9c029140a642648b985c067
Lenny Lopes <lennymlopes@gmail.com>
2021-05-02 12:18:23 +0200

    adjust fade in timing

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', () => {
   let position = window.innerHeight
   for(section of sections) {
     
-    if(section.getBoundingClientRect().bottom / 0.8 < position) {
+    if(section.getBoundingClientRect().top / 0.6 < position) {
       section.classList.add('active')
     } 
     else if (section.getBoundingClientRect().top / 0.7 > position) {
