3ff96cd536a0271627f14c5268a04eeecf6a0aa1
Lenny Lopes <lennymlopes@gmail.com>
2021-05-02 22:04:58 +0200

    fiddle with fade threshold

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', () => {
   let sections = document.querySelectorAll('.fade')
   let position = window.innerHeight
   for(section of sections) {
-    if(section.getBoundingClientRect().bottom / 0.9 < position) {
+    if(section.getBoundingClientRect().top / 0.7 < position) {
       section.classList.add('active')
     } 
     else if (section.getBoundingClientRect().top / 0.5 > position) {
