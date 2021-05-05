ff420523e6c6552ddbc811abe4a5f41c96bdff30
Lenny Lopes <lennymlopes@gmail.com>
2021-05-05 12:42:43 +0200

    configure eta

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const compression = require('compression')
 const path = require('path')
 const eta = require('eta')
+const languages = require('./languages.json')
 
 const app = express()
 app.use(compression())
@@ -9,11 +10,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static(path.join(__dirname, './client', 'static')))
 
+app.engine("eta", eta.renderFile)
+app.set("view engine", "eta")
+app.set("views", "./client/views")
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   const userLanguages = req.acceptsLanguages()
-  res.sendFile(path.join(__dirname, './client', 'index.html'))
+  res.render("index", languages["de"])
 })
 
 app.listen(PORT, () => {
