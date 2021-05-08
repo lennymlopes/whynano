781d4ab51844bf43c54ab0586d23410428f3afdd
Lenny Lopes <lennymlopes@gmail.com>
2021-05-08 09:14:51 +0200

    npx standard --fix, remove unneded function

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -1,11 +1,10 @@
 window.addEventListener('scroll', () => {
-  let sections = document.querySelectorAll('.fade')
-  let position = window.innerHeight
-  for(section of sections) {
-    if(section.getBoundingClientRect().top / 0.7 < position) {
+  const sections = document.querySelectorAll('.fade')
+  const position = window.innerHeight
+  for (section of sections) {
+    if (section.getBoundingClientRect().top / 0.7 < position) {
       section.classList.add('active')
-    } 
-    else if (section.getBoundingClientRect().top / 0.5 > position) {
+    } else if (section.getBoundingClientRect().top / 0.5 > position) {
       section.classList.remove('active')
     }
   }
@@ -18,28 +17,14 @@ aboutButton.addEventListener('click', () => {
   aboutSection.classList.toggle('hidden')
 })
 
-
-function setCookie(name, value) {
-  const YEAR = 60*60*24*365
+function setCookie (name, value) {
+  const YEAR = 60 * 60 * 24 * 365
   document.cookie = `${name}=${value}; max-age=${YEAR}; path=/`
-}
-
-function getCookies(name='') {
-  let cookies = {}
-  for(cookie of document.cookie.split(';')) {
-    let [key, value] = cookie.split('=')
-    cookies[key] = value
-  }
-  if(name) {
-    // return value if cookie exist, return undefined otherwise
-    return Object.keys(cookies).includes(name) ? cookies[name] : undefined
-  }
-  return cookies
 }
 
 const languagePicker = document.querySelector('#language-picker-select')
 languagePicker.addEventListener('change', () => {
-  let selectedLanguage = languagePicker.selectedOptions[0].getAttribute("lang")
-  setCookie("selectedLanguage", selectedLanguage)
+  const selectedLanguage = languagePicker.selectedOptions[0].getAttribute('lang')
+  setCookie('selectedLanguage', selectedLanguage)
   location.reload()
 })
--- index.js
+++ index.js
@@ -10,43 +10,43 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static(path.join(__dirname, './client', 'static')))
 
-app.engine("eta", eta.renderFile)
-app.set("view engine", "eta")
-app.set("views", "./client/views")
+app.engine('eta', eta.renderFile)
+app.set('view engine', 'eta')
+app.set('views', './client/views')
 
 const PORT = process.env.PORT || 5000
 
-let languages = {}
-let availableLanguages = []
+const languages = {}
+const availableLanguages = []
 fs.readdir('./languages', (err, filenames) => {
   console.log(filenames)
-  for(file of filenames) {
+  for (file of filenames) {
     fs.readFile('./languages/' + file, 'utf-8', (err, content) => {
       content = JSON.parse(content)
       languages[content.iso] = content
-      availableLanguages.push({iso: content.iso, value: content.value, display_name: content.display_name })
+      availableLanguages.push({ iso: content.iso, value: content.value, display_name: content.display_name })
     })
   }
 })
 
 app.get('/', (req, res) => {
   const userLanguages = req.acceptsLanguages()
-  let cookies = parseCookies(req.headers.cookie)
-  let selectedLanguage = Object.keys(cookies).includes('selectedLanguage') ? cookies['selectedLanguage'] : 'en'
-  res.render("index", {...languages[selectedLanguage], availableLanguages})
+  const cookies = parseCookies(req.headers.cookie)
+  const selectedLanguage = Object.keys(cookies).includes('selectedLanguage') ? cookies.selectedLanguage : 'en'
+  res.render('index', { ...languages[selectedLanguage], availableLanguages })
 })
 
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`)
 })
 
-function parseCookies(raw) {
-  if(!raw) {
+function parseCookies (raw) {
+  if (!raw) {
     return []
   }
-  let cookies = {}
-  for(cookie of raw.split('; ')) {
-    let [key, value] = cookie.split('=')
+  const cookies = {}
+  for (cookie of raw.split('; ')) {
+    const [key, value] = cookie.split('=')
     cookies[key] = value
   }
   return cookies
