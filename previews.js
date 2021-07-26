fe565755c271bfe7544cae387e6ff6d3fba5ba55
Lenny Lopes <lennymlopes@gmail.com>
2021-07-26 08:21:11 +0200

    smaller viewport, larger resolution

--- previews.js
+++ previews.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 async function create() {
   
   
-  let width = 800
+  let width = 600
   let height = Math.floor(width / 1.91)
   console.log(`${width}x${height}`)
   
@@ -19,10 +19,10 @@ async function create() {
         await page.setViewport({
           width: width,
           height: height,
-          deviceScaleFactor: 1,
+          deviceScaleFactor: 2,
         })
-        await page.goto(`http://localhost:5000/${iso}`);
-        console.log()
+        await page.goto(`https://whynano.cc/${iso}`);
+        // console.log()
         await page.screenshot({ path: `./client/static/img/preview_${iso}.png`  });
         await browser.close();
       })
